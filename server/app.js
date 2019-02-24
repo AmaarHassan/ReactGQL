cf19a818327b6710e9f71a0957c727d35e7426a8
AmaarHassan <amaarhassancs@gmail.com>
2019-02-25 00:12:51 +0500

    Removing all unnecessary changes from the front end

--- server/app.js
+++ server/app.js
@@ -4,8 +4,16 @@ const schema = require('./schema/schema.js');
 const mongoose = require('mongoose');
 
 const app = express();
- 
-mongoose.connect("mongodb://amr:123@cluster0-shard-00-00-gcrtg.mongodb.net:27017,cluster0-shard-00-01-gcrtg.mongodb.net:27017,cluster0-shard-00-02-gcrtg.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true");
+
+mongoose.connect("mongodb://amr:123@cluster0-shard-00-00-gcrtg.mongodb.net:27017,cluster0-shard-00-01-gcrtg.mongodb.net:27017,cluster0-shard-00-02-gcrtg.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", function() { /* dummy function */ })
+.then(() => {
+    console.log('Server should start here');
+})
+.catch(err => { // mongoose connection error will be handled here
+    console.error('App starting error:', err.stack);
+    process.exit(1);
+});
+
 mongoose.connection.once('open', () =>{
     console.log('Connected to database');
 });
