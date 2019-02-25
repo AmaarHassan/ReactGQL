222eab87dd1ce822621015027ec86ae4fb1c2f86
AmaarHassan <amaarhassancs@gmail.com>
2019-02-25 15:23:35 +0500

    setting up for book list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+//components
+import BookList from './components/BookList';
+
 class App extends Component {
   render() {
     return (
       <div id="main">
        <h1> Welcome to the World of React </h1>
+        <h2 ><BookList/> </h2>
       </div>
     );
   }
