5f7ab79d1a745948db9d600c8121542bb9f88aab
AmaarHassan <amaarhassancs@gmail.com>
2019-02-26 13:00:48 +0500

    BookDetails component is expecting props(id) from the Parent component. The query expects the id, runs as it loads, displays the result in html

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -5,14 +5,56 @@ import {graphql} from 'react-apollo';
 import {getBookDetailsQuery} from '../queries/queries';
 
 class BookDetails extends Component {
-  render() {
+    displayBookDetails(){
+        if(this.props.data.loading){
+            return(
+                <div>
+                    <p> ...Getting Details... </p>
+                </div>
+            )
+        }
+        const {book} = this.props.data;
+        if(book){
+            return(
+                <div>
+                    <h2> {book.name} </h2>
+                    <p> {book.genre} </p>
+                    <p> {book.author.name} </p>
+                    <p> All books by the author</p>
+                    <ul className="other-books">
+                        {
+                            book.author.books.map(item =>{
+                                return <li key={item.id}> {item.name} </li> 
+                            })
+                        }
+                    </ul>
+                </div>
+            );
+        }else{
+          return(
+              <div>
+                    <h3> No book selected </h3>
+              </div>
+          )
+        }
+    }
+    render() {
+    
     return (
         <div id="book-details">
-            <p> {this.props.id} </p>
+            {this.displayBookDetails()}
         </div>
     );
   }
 }
 
-// bind query with component and export it
-export default graphql(getBookDetailsQuery)(BookDetails);
+// since the query is expecting an id, we register it into options
+export default graphql(getBookDetailsQuery,{
+    options: (props)=>{
+        return{
+            variables: {
+                id: props.id
+            }
+        }
+    }
+})(BookDetails);
